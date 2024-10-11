63ca89f3e08c89687d7001f0931f686c6c7df879
John Torakis <john.torakis@gmail.com>
2024-10-11 14:06:33 +0300

    split out State to different file

---
+++ assets/js/state.js
@@ -0,0 +1,35 @@
+STATE = {
+  "players": {
+    0: {
+      "hand": [],
+      "turnStrategy": [],
+      "phase" : GAMEPHASES.SETUP,
+      "message": "",
+      "hasParcel": false,
+      "score": 0,
+    },
+    1: {
+      "hand": [],
+      "turnStrategy": [],
+      "phase" : GAMEPHASES.SETUP,
+      "message": "",
+      "hasParcel": false,
+      "score": 0,
+    },
+  },
+  "positions": {
+    "parcel": {"x":-1, "y":-1}, //they all need to be -1
+    "goal": {"x":-1, "y":-1},
+    0: {"x":-1, "y":-1},
+    1: {"x":-1, "y":-1},
+  },
+  "decks": {
+    "action": [],
+    "direction": [],
+    "trunk": [],
+  },
+  "ctx": {
+    "currentPlayer": 0,
+    "turn": 0,
+  },
+}
--- main.js
+++ main.js
@@ -6,42 +6,7 @@ class NetPlayJSGame extends netplayjs.Game {
   constructor(canvas, players) {
     super();
 
-    this.G = {
-      "players": {
-        0: {
-          "hand": [],
-          "turnStrategy": [],
-          "phase" : GAMEPHASES.SETUP,
-          "message": "",
-          "hasParcel": false,
-          "score": 0,
-        },
-        1: {
-          "hand": [],
-          "turnStrategy": [],
-          "phase" : GAMEPHASES.SETUP,
-          "message": "",
-          "hasParcel": false,
-          "score": 0,
-        },
-      },
-      "positions": {
-        "parcel": {"x":-1, "y":-1}, //they all need to be -1
-        "goal": {"x":-1, "y":-1},
-        0: {"x":-1, "y":-1},
-        1: {"x":-1, "y":-1},
-      },
-      "decks": {
-        "action": [],
-        "direction": [],
-        "trunk": [],
-      },
-      "ctx": {
-        "currentPlayer": 0,
-        "turn": 0,
-      },
-    }
-
+    this.G = STATE;
     this.players = players;
   }
 
