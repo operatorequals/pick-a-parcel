b9485460df70e1b774f2df3d4f3868b9ef96d5f2
Charalampos Leontidis <chleontidis@yahoo.com>
2024-10-25 18:11:27 +0300

    change vw for em and make card size relative to parent height

--- src/components/game/Card.js
+++ src/components/game/Card.js
@@ -5,42 +5,42 @@ import './CardAnimation.css';
 import { GAMEPHASES, CONSTANTS, EVENTS } from '../../game/constants';
 
 
-const FrontContent = ({type, value}) => {
-  if (type==="action")
-    return <div className="card-front card">
-      <h2><span className="card-value-action">{value}</span><span className="card-action-parenthesis">(
-        <br/>
-        <br/>
-      )</span><span className="card-action-semicolon">;</span></h2>
-      <p className="card-type"># function</p>
-    </div>
-  else 
-    return <div className="card-front card-direction card">
-    <h2><span className="card-value-direction">
-      <br/>"{value}"</span>
-    </h2>
-    <p className="card-type"># parameter</p>
-</div>
+const FrontContent = ({ type, value }) => {
+    if (type === "action")
+        return <div className="card-front card">
+            <h2>
+                <span className="card-value-action">{value}<span className="card-action-parenthesis">(</span></span>
+                <span className="card-action-semicolon"><span className="card-action-parenthesis">)</span>;</span>
+            </h2>
+            <div className="card-type"># function</div>
+        </div>;
+    else
+        return <div className="card-front card-direction card">
+            <h2><span className="card-value-direction">
+                <br />"{value}"</span>
+            </h2>
+            <div className="card-type"># parameter</div>
+        </div>;
 
-}
+};
 
 const BackContent = () => <div className="card-back card">
     <p className="card-back-text">pick(
-    "parcel"
-    <br/>
-    );</p>
-    </div>
+        "parcel"
+        <br />
+        );</p>
+</div>;
 
 export const Card = ({ id, type, value, face, onclick, animation }) => {
 
-  return (
-    <div className={`cardWrapper ${animation ? "card-animation" : ""}`} onClick={()=>onclick(id)}>
-        <div className="card-inner">
-        {(face === "up") ?
-          <FrontContent type={type} value={value} /> : <BackContent />
-        }
+    return (
+        <div className={`cardWrapper ${animation ? "card-animation" : ""}`} onClick={() => onclick(id)}>
+            <div className="card-inner">
+                {(face === "up") ?
+                    <FrontContent type={type} value={value} /> : <BackContent />
+                }
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
