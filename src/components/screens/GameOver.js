74dee1ee5f573528d0a547908390bd97c200a6a8
Charalampos Leontidis <chleontidis@yahoo.com>
2024-10-25 16:40:55 +0300

    feat: gameover screen beautiful

--- src/components/screens/GameOver.js
+++ src/components/screens/GameOver.js
@@ -1,39 +1,41 @@
 import { maxMatchID } from '../../WebAppConstants';
-
+import './GameOver.css';
 
 // import './GameOver.css';
 
-export const GameOver = ({G, ctx, reset, playerID, matchID}) => {
+export const GameOver = ({ G, ctx, reset, playerID, matchID }) => {
 
-	const winner = ctx.gameover.winner;
-	const reason = ctx.gameover.reason;
-	const draw = winner === undefined;
-	const won = Number(winner) === Number(playerID);
+    const winner = ctx.gameover.winner;
+    const reason = ctx.gameover.reason;
+    const draw = winner === undefined;
+    const won = Number(winner) === Number(playerID);
 
-	const fullPath = window.location.origin + window.location.pathname
-	const replayerMatchID = Number(matchID) * 87634 % maxMatchID // calculate the same next ID
+    const fullPath = window.location.origin + window.location.pathname;
+    const replayerMatchID = Number(matchID) * 87634 % maxMatchID; // calculate the same next ID
 
-	let params = new URLSearchParams()
-	params.set('matchID', replayerMatchID)
-	params.set('isHost', playerID === '0' ? 'true' : 'false')
+    let params = new URLSearchParams();
+    params.set('matchID', replayerMatchID);
+    params.set('isHost', playerID === '0' ? 'true' : 'false');
 
-	const replayURL = `${fullPath}#?${params.toLocaleString()}`;
+    const replayURL = `${fullPath}#?${params.toLocaleString()}`;
 
-	return (
-		<div>
-			<div className="result-text">
-			{draw ? 
-				"It's a DRAW" :
-				(won ? "You WON!" :
-				"You LOST")
-			}
-			</div>
-			{ !draw ?
-			<div className="result-points">
-				Points acquired: {ctx.gameover.points}!
-			</div>
-			: ""}
-			<a href={replayURL}>RESET!</a>
-		</div>
-		)
-}
+    return (
+        <div className='gameover-container'>
+            <div className='text-container'>
+                <div className="result-text">
+                    {draw ?
+                        "It's a DRAW" :
+                        (won ? "You WON!" :
+                            "You LOST")
+                    }
+                </div>
+                {!draw ?
+                    <div className="result-points">
+                        Points acquired: <span>{ctx.gameover.points}</span>!
+                    </div>
+                    : ""}
+                <a className='reset-button' href={replayURL}>playAgain<span>()</span>;</a>
+            </div>
+        </div>
+    );
+};
