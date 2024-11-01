95ef39c4f47efa39b6e994a35b328cf60a055fcb
John Torakis <john.torakis@gmail.com>
2024-11-01 17:22:50 +0200

    feat: implement input matchID

--- src/components/matchmaking/P2P.js
+++ src/components/matchmaking/P2P.js
@@ -1,40 +1,82 @@
+import { useState } from 'react';
 import {QRCodeSVG} from 'qrcode.react';
 
 import './P2P.css'; // Optional: for styling
+import '../pages/ui.css'; // Optional: for styling
+
+
+
 
 
 export const P2PQRCode = ({ matchID, matchIDPrefix }) => {
 
+	const [inputMatchID, setInputMatchID] = useState(null);
+
 	matchIDPrefix = matchIDPrefix ? matchIDPrefix : 'pick-a-parcel-'
 	matchIDPrefix = ''
 
-	const fullPath = window.location.origin + window.location.pathname
-	let params = new URLSearchParams()
-	params.set('matchID', matchIDPrefix+matchID)
-	params.set('isHost', false)
+	const getMatchURL = (matchID, isHost) => {
+		let params = new URLSearchParams()
+		params.set('matchID', matchID)
+		params.set('isHost', isHost)
+		const fullPath = window.location.origin + window.location.pathname
 
-	const matchUrl = `${fullPath}#?${params.toLocaleString()}`;
+		return `${fullPath}#/?${params.toLocaleString()}`;
+	}
+
+	const matchUrl = getMatchURL(matchIDPrefix+matchID, false)
 
 	return (
-	<div className='page-matchmaking'>
-	<div className='matchmaking'>
-		<h1> Welcome to <span className="gametitle">Pick-A-Parcel</span> Multiplayer</h1>
-		<h2 className="matchmaking-text"> Your match ID is: <code className="matchmaking-matchid">{matchID}</code> </h2>
+<div className='page-matchmaking'>
+	<div className='ui-bubble'>
+		<h2> Welcome to <span className="gametitle">Pick-A-Parcel</span> Multiplayer</h2>
+		<h3 style={{textAlign: "center"}}> Your match ID is: <code className="matchmaking-matchid">{matchID}</code> </h3>
+		<h4 style={{textAlign: "center"}}> Get someone to join the game by:</h4>
 		<div className="matchmaking-options">
 			<div className="matchmaking-option matchmaking-qrcode">
-				<p>
-				Get someone to join the game by scanning the QRCode:
-				</p>
+				<span className="ui-text">
+				scanning the QRCode:
+				</span>
+				{/* --sublime-white, --sublime-gray*/}
 				<QRCodeSVG value={matchUrl} includeMargin={true}
 					bgColor="#f8f8f2" fgColor="#43443b"
 				/>
 			</div>
-			<div className="matchmaking-option matchmaking-link"> or by clicking on:<br/>
+			<div className="matchmaking-option matchmaking-link">
+				<span className="ui-text">
+				sharing this link:
+				</span>
 				<a className='matchmaking' href={matchUrl} target="_blank">{matchUrl}</a>
+			</div>
+
+			<div className="matchmaking-option matchmaking-input">
+				<span className="ui-text">
+				Or join yourself a game by typing a <code>Match ID</code> here:
+				</span>
+				<form className="matchmaking-input-form"
+					onSubmit={(e)=>{
+							e.preventDefault();
+							if (inputMatchID === null || inputMatchID.match(/^ *$/)) return
+							const url = getMatchURL(inputMatchID, false)
+							console.log(url)
+							window.location=url
+							}
+						}>
+					<input type="text" name="matchID"
+						maxLength="8" size="8"
+						onChange={e => setInputMatchID(e.target.value)} />
+					{/* <input type="hidden" name="isHost" value="false" onChange={e => setInputMatchID(e.target.value)} /> */}
+					<button type="submit" disabled={
+						(inputMatchID === null || inputMatchID.match(/^ *$/)) ? true : false
+						}>
+							Play Game!
+					</button>
+				</form>
+
 			</div>
 		</div>
 	</div>
-	</div>
+</div>
 	);
 
 }
--- src/components/screens/Loading.js
+++ src/components/screens/Loading.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import '../pages/ui.css'
 import './Loading.css'
 
@@ -12,13 +14,24 @@ export const Loading = ({}) => {
 	const params = new URLSearchParams(location.search);
 	const matchID = params.get('matchID');
 
-	return(
+	const [tooLong, setTooLong] = useState(false)
+	const tooLongTimer = 5*1000 // 10 secs
+	setTimeout(()=>{
+		setTooLong(true)
+	}, tooLongTimer)
 
+	return(
 <div className="page-loading">
 	<div className="ui-bubble" style={{textAlign: "center"}}>
 		<h2> Connecting to Game... </h2>
-		{matchID !== null ?? <h3> Match ID: {matchID} </h3>}
-		<h4>Good Luck!</h4>
+		{matchID !== null && <h3> Match ID: {matchID} </h3>}
+		{ tooLong
+			? <div>
+				<h4> This is taking way too long... </h4>
+				Maybe try refreshing the page?
+			</div>
+			: <h4>Good Luck!</h4>
+		}
 	</div>
 	<InfoBubble />
 </div>
