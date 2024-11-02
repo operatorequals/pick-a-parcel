3c146da5b599b94f82657c4e92fa8e8e9825e20e
John Torakis <john.torakis@gmail.com>
2024-11-02 18:03:19 +0200

    chore: some URL constants

--- src/WebAppConstants.js
+++ src/WebAppConstants.js
@@ -5,3 +5,17 @@ export const maxMatchID = 10000;
 export const generateMatchID = () => matchIDPrefix+(Math.round(Math.random() * maxMatchID)).toString();
 
 export const testingMultiplayer = true
+
+export const hashRouting = true;
+
+const baseUrl = (process.env.PUBLIC_URL.startsWith("/") ?
+	window.location.origin + process.env.PUBLIC_URL :
+	process.env.PUBLIC_URL) + hashRouting ? "/#" : ""
+
+// console.log(baseUrl)
+export const urls = {
+	"base": baseUrl,
+	"how-to-play": baseUrl + "/how-to-play",
+	"tutorial": baseUrl + "/tutorial",
+
+}
